6fa0ab8d78c332e2b02f5ec92317b2cf6e84891f
Benjamin Everest <everest.bt@gmail.com>
2026-09-11 08:28:36 +0100

    Add util achievement object + method to get LP

--- magic_the_gathering_arena/magic_the_gathering_arena_utils/src/lib.rs
+++ magic_the_gathering_arena/magic_the_gathering_arena_utils/src/lib.rs
@@ -1,13 +1,24 @@
 use anyhow::Result;
 use magic_the_gathering_arena_log_reader::{
-    get_readable_achievements
+    get_readable_achievements,
 };
 use magic_the_gathering_arena_db::achievement_store::{
     self, Achievement, save_achievement
 };
+use jiff::Timestamp;
 
 pub const GAME_NAME : &str = "Magic the Gathering Arena";
 pub const ID : i32 = 0;
+
+#[derive(Eq, PartialEq, Ord, PartialOrd, Clone)]
+pub struct MTGAAchievement {
+    pub name: String, 
+    pub achieved: bool,
+}
+
+fn from_db_format(a: &Achievement) -> MTGAAchievement {
+    MTGAAchievement { name: a.name.clone(), achieved: a.achieved }
+}
 
 pub fn sync_achievements() -> Result<()> {
     let achievements = get_readable_achievements();
@@ -19,6 +30,20 @@ pub fn sync_achievements() -> Result<()> {
     Ok(())
 }
 
-pub fn get_achievements() -> Result<Vec<Achievement>> {
-    Ok(achievement_store::get_achievements()?)
+pub fn get_achievements() -> Result<Vec<MTGAAchievement>> {
+    Ok(achievement_store::get_achievements()?
+        .iter()
+        .map(|a| from_db_format(a))
+        .collect())
+}
+
+pub fn get_goals() -> Result<Vec<MTGAAchievement>> {
+    Ok(achievement_store::get_goals()?
+        .iter()
+        .map(|a| from_db_format(a))
+        .collect())
+}
+
+pub fn get_last_played_time() -> Option<Timestamp> {
+    magic_the_gathering_arena_log_reader::get_last_played_time()
 }
